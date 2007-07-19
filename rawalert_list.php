9673515b8a2c1d8b53ebe4e93c0e318df394633c
yuli_s <yuli_s@7e47f64f-3c34-0410-9091-801b9fe948be>
2007-07-19 16:23:41 +0000

    Added Raw Alerts Windows.
    git-svn-id: http://svn.code.sf.net/p/greensql/code/trunk/greensql-console@28 7e47f64f-3c34-0410-9091-801b9fe948be

---
+++ rawalert_list.php
@@ -0,0 +1,56 @@
+<?php
+
+require 'lib.php';
+
+require 'libs/Smarty.class.php';
+
+$error = "";
+$msg = "";
+
+$start_id = intval($_GET[p]);
+$limit_per_page = 10;
+
+$smarty = new Smarty;
+$smarty->compile_check = true;
+
+$smarty->assign("Name","View alerts");
+$smarty->assign("Page","rawalert_list.tpl");
+
+$dbs = get_databases();
+$smarty->assign("databases", $dbs);
+
+$status = 0;
+$alerts = get_raw_alerts_bypage($start_id*$limit_per_page, $limit_per_page, $status);
+$smarty->assign("alerts", $alerts);
+
+$numResults = get_num_raw_alerts($status);
+
+  $file = 'rawalert_list.php';
+  // update list of pages
+  $num_pages = ceil($numResults/$limit_per_page)+1;
+  if ($start_id > 2)
+    $from_id = $start_id - 1;
+  else
+    $from_id = 1;
+  $to_id = $from_id + 5;
+
+  if ($start_id > 1)
+    $list_pages .= '<a href="'.$file.'?p='.($start_id-1).'">Previous</a>&nbsp;';
+  else if ($start_id == 1)
+    $list_pages .= '<a href="'.$file.'">Previous</a>&nbsp;';
+    
+  for ($i = $from_id; $i < $num_pages && $i < $to_id; $i++)
+  {
+    if (($i-1) == $start_id)
+      $list_pages .= '<b>'.$i . '</b>&nbsp;';
+    else
+      $list_pages .= '<a href="'.$file.'?p='.($i-1).'">'.$i.'</a>&nbsp;';
+  }
+  if ($start_id < $num_pages-2)
+    $list_pages .= '<a href="'.$file.'?p='.($start_id+1).'">Next</a>&nbsp;';
+  $list_pages .= '<br/>';
+
+$smarty->assign("pager", $list_pages);
+$smarty->display('index.tpl');
+
+?>
