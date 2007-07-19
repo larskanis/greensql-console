99115ccbb4acba53c8d804c1dbbea4f2c231fd9e
yuli_s <yuli_s@7e47f64f-3c34-0410-9091-801b9fe948be>
2007-07-19 16:34:01 +0000

    Added code to display alert by pages.
    git-svn-id: http://svn.code.sf.net/p/greensql/code/trunk/greensql-console@36 7e47f64f-3c34-0410-9091-801b9fe948be

--- alert_list.php
+++ alert_list.php
@@ -12,17 +12,55 @@ if (!$status)
 {
     $status = 0;
 }
+$start_id = intval($_GET[p]);
+$limit_per_page = 10;
+
 $smarty = new Smarty;
 $smarty->compile_check = true;
 
-$smarty->assign("Name","List of Alerts");
+if ($status == 1)
+{
+  $smarty->assign("Name","Whitelist of approved queries");
+} else {
+  $smarty->assign("Name","List of Alerts");
+}
 $smarty->assign("Page","alert_list.tpl");
 
 $dbs = get_databases();
 $smarty->assign("databases", $dbs);
+$smarty->assign("status", $status);
 
-$alerts = get_alerts($status);
+$alerts = get_alerts_bypage($start_id,$limit_per_page,$status);
 $smarty->assign("alerts", $alerts);
+
+$numResults = get_num_alerts($status);
+
+  $file = "alert_list.php?status=$status&";
+  // update list of pages
+  $num_pages = ceil($numResults/$limit_per_page)+1;
+  if ($start_id > 2)
+    $from_id = $start_id - 1;
+  else
+    $from_id = 1;
+  $to_id = $from_id + 5;
+
+  if ($start_id > 1)
+    $list_pages .= '<a href="'.$file.'p='.($start_id-1).'">Previous</a>&nbsp;';
+  else if ($start_id == 1)
+    $list_pages .= '<a href="'.$file.'">Previous</a>&nbsp;';
+
+  for ($i = $from_id; $i < $num_pages && $i < $to_id; $i++)
+  {
+    if (($i-1) == $start_id)
+      $list_pages .= '<b>'.$i . '</b>&nbsp;';
+    else
+      $list_pages .= '<a href="'.$file.'p='.($i-1).'">'.$i.'</a>&nbsp;';
+  }
+  if ($start_id < $num_pages-2)
+    $list_pages .= '<a href="'.$file.'p='.($start_id+1).'">Next</a>&nbsp;';
+  $list_pages .= '<br/>';
+
+$smarty->assign("pager", $list_pages);
 
 $smarty->display('index.tpl');
 
