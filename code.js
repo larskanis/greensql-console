66cc1eb72e1c311701f75261f10c853ddad8f86c
yuli_s <yuli_s@7e47f64f-3c34-0410-9091-801b9fe948be>
2007-07-19 16:26:50 +0000

    Added JavaScript file.
    git-svn-id: http://svn.code.sf.net/p/greensql/code/trunk/greensql-console@31 7e47f64f-3c34-0410-9091-801b9fe948be

---
+++ code.js
@@ -0,0 +1,66 @@
+function init_help()
+{
+var help_win = document.getElementById('help_content');
+var show_win = document.getElementById('show_help');
+var show = getCookie('show_help');
+if (show == 1 || show == "")
+{
+  setCookie('show_help','1',10);
+  if (help_win && show_win)
+  {
+    help_win.style.display='block';
+    show_win.style.display='none';
+  }
+} else {
+  setCookie('show_help','0',10);
+  if (help_win && show_win)
+  {
+    help_win.style.display='none';
+    show_win.style.display='block';
+  }
+}
+}
+
+function setCookie(c_name,value,expiredays)
+{
+var exdate=new Date();
+exdate.setDate(exdate.getDate()+expiredays);
+document.cookie=c_name+ "=" +escape(value)+
+ ((expiredays==null) ? "" : ";expires="+exdate.toGMTString());
+}
+
+function getCookie(c_name)
+{
+if (document.cookie.length>0)
+  {
+  c_start=document.cookie.indexOf(c_name + "=")
+  if (c_start!=-1)
+    { 
+    c_start=c_start + c_name.length+1 
+    c_end=document.cookie.indexOf(";",c_start)
+    if (c_end==-1) c_end=document.cookie.length
+    return unescape(document.cookie.substring(c_start,c_end))
+    } 
+  }
+return "";
+}
+
+function show_help()
+{
+var help_win = document.getElementById('help_content');
+var show_win = document.getElementById('show_help');
+
+help_win.style.display='block';
+show_win.style.display='none';
+setCookie('show_help','1',10);
+}
+
+function hide_help()
+{
+var help_win = document.getElementById('help_content');
+var show_win = document.getElementById('show_help');
+
+help_win.style.display='none';
+show_win.style.display='block';
+setCookie('show_help','0',10);
+}
