879bce1885291d0ad6b990ead087fe40dbbc4912
yuli_s <yuli_s@7e47f64f-3c34-0410-9091-801b9fe948be>
2007-12-01 08:48:16 +0000

    Added comments.
    git-svn-id: http://svn.code.sf.net/p/greensql/code/trunk/greensql-console@112 7e47f64f-3c34-0410-9091-801b9fe948be

--- config.php
+++ config.php
@@ -3,13 +3,23 @@
 # Uncomment the following line to switch to demo version
 #$demo_version = 1;
 
+# MySQL Database IP address
 $db_host = "127.0.0.1";
+
+#MySQL Database Port Value.
 $db_port = 3306;
 
+# MySQL database name used to store greensql confiuration and alerts
 $db_name = "greendb";
+
+# MySQL database user and password
 $db_user = "green";
 $db_pass = "pwd";
 
+# If you run greensql-fw service on the same computer you can specify
+# location of it's log file. It will be visible as part of the console.
 $log_file = "/var/log/greensql.log";
 
+# Number of lines to show when viewing log file.
+$num_log_lines = 200;
 ?>
