7168f70d9ac1279697937b7269ec55fdc50c11dd
yuli_s <yuli_s@7e47f64f-3c34-0410-9091-801b9fe948be>
2008-04-05 07:10:09 +0000

    Display application version in the footer.
    git-svn-id: http://svn.code.sf.net/p/greensql/code/trunk/greensql-console@184 7e47f64f-3c34-0410-9091-801b9fe948be

--- lib.php
+++ lib.php
@@ -1,8 +1,5 @@
 <?php
 
-# Application version.
-$greensql_console_ver = "0.3.2";
-
 #set default time zone - this prevents PHP5 from 
 #showing strange warning messages
 date_default_timezone_set("America/Los_Angeles");
@@ -24,6 +21,8 @@ $smarty = new Smarty;
 $smarty->compile_check = true;
 global $cache_dir;
 $smarty->compile_dir = $cache_dir;
+global $version;
+$smarty->assign("Version", $version);
 #$smarty->debugging = true;
 
 #if ( ($error = db_connect()) != "")
--- templates/footer.tpl
+++ templates/footer.tpl
@@ -1,6 +1,7 @@
 <br/>
 <center>
 <tt><div id=menu><a href="http://www.greensql.net/">GreenSQL Open Source Database Firewall</a></div></tt>
+<small>GreenSQL-Console ver. {$Version}</small>
 </center>
 </BODY>
 </HTML>
