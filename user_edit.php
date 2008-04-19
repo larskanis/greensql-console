f553ee7561a15e5a2a4b2372b0d083ff5090e80e
yuli_s <yuli_s@7e47f64f-3c34-0410-9091-801b9fe948be>
2008-04-19 19:30:18 +0000

    Fixed XSS bug. Fixed a number of spelling errors.
    git-svn-id: http://svn.code.sf.net/p/greensql/code/trunk/greensql-console@202 7e47f64f-3c34-0410-9091-801b9fe948be

--- user_edit.php
+++ user_edit.php
@@ -18,43 +18,58 @@ $msg = "";
 if (isset($_POST['submit']))
 {
     #data posted, db need to be updated
-    $user['name'] = trim($_POST['name']);
-    $user['email'] = trim($_POST['email']);
+    $user['name'] = trim(htmlspecialchars($_POST['name']));
+    $user['email'] = trim(htmlspecialchars($_POST['email']));
     
     if (strlen($user['name']) == 0)
     {
         $error .= "User can not be empty.<br/>\n";
+    } else if (!ereg("^[a-zA-Z0-9_]+$", $user['name']))
+    {
+        $error .= "Username is invalid. It contains illegal characters. Valid characters are a-z, A-Z, 0-9 and '_'.<br/>\n";
     }
     if (strlen($user['email']) == 0)
     {
         $error .= "Email can not be empty.<br/>\n";
+    } else if (!ereg("^[a-zA-Z0-9_i\@]+$", $user['email']))
+    {
+        $error .= "Email address is invalid. It contains illegal characters. Valid characters are a-z, A-Z, 0-9, '_' and '@'.<br/>\n";
     }
-    $oldpass = sha1(trim($_POST['oldpass']));
+    $_POST['oldpass'] = htmlspecialchars(trim($_POST['oldpass']));
+    $oldpass = sha1($_POST['oldpass']);
     if ($user['pwd'] != $oldpass)
     {
         $error .= "Old password is wrong. Please retype.<br/>\n";
+    } else if (!ereg("^[a-zA-Z0-9_]+$",$_POST['oldpass']))
+    {
+        $error .= "Old password is invalid. It contains illegal characters. Valid characters are a-z, A-Z, 0-9 and '_'.<br/>\n";
     }
-    $pass = trim($_POST['pass']);
-    $pass2 = trim($_POST['pass2']);
+
+    $pass = trim(htmlspecialchars($_POST['pass']));
+    $pass2 = trim(htmlspecialchars($_POST['pass2']));
     if (strlen($pass) == 0)
     {
         $error .= "New password can not be empty.<br/>\n";
+    } else if (!ereg("^[a-zA-Z0-9_]+$",$pass))
+    {
+        $error .= "New password is invalid. It contains illegal characters. Valid characters are a-z, A-Z, 0-9 and '_'.<br/>\n";
     }
+
     if ($pass != $pass2)
     {
-        $error .= "New passwords are different.<br/>\n";
+        $error .= "The new password does not match. Please try again.<br/>\n";
     }
-    $user['pwd'] = $pass;
     if ($demo_version)
     {
         $error .= "You can not change passwords in demo version.";
     }
     else if (!$error)
     {
+        $user['pwd'] = $pass;
         $error = update_user($user);
 	if (!$error)
 	{
-	    $msg = "Database has been succesfully updated.";
+	    $msg = "Database has been successfully updated.";
 	}
     }
     if ($error)
