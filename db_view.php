601a92cc1658e835e0caab53105c597081282899
yuli_s <yuli_s@7e47f64f-3c34-0410-9091-801b9fe948be>
2008-09-09 19:15:21 +0000

    Added simulation and learning modes.
    git-svn-id: http://svn.code.sf.net/p/greensql/code/trunk/greensql-console@313 7e47f64f-3c34-0410-9091-801b9fe948be

--- db_view.php
+++ db_view.php
@@ -30,7 +30,7 @@ $smarty->assign("DB_ID", $db_id);
 
 $enabled_str = "<font color=\"red\">enabled</font>";
 
-$smarty->assign("DB_StatusId", $db['status']);
+$smarty->assign("DB_StatusId", $db['proxy_status']);
 if ($db['status'] == 1)
 {
     $smarty->assign("DB_Status", "<font color=\"green\">Listener is 'OK'</font>");
@@ -40,36 +40,64 @@ if ($db['status'] == 1)
     $smarty->assign("DB_Status", "Listener is 'UNKNOWN'");
 }
     
+$block_status = '';
 
-if ($db['alter_perm'] == 0)
+if ($db['status'] == 0)
+{
+    $block_status = '1. Block queries based on the risk calculation and usage of privileged operations.';
+} else if ($db['status'] == 1)
+{
+    $block_status = '2. Sollely block queries that use privileged operations. Risk calculation is not taken into account.';
+} else if ($db['status'] == 2)
+{
+    $block_status = '3. Simulation mode. <b>Nothing is blocked.</b>';
+} else if ($db['status'] == 3)
+{
+    $block_status = '4. Simulatate blocking of privileged commands.\n';
+} else if ($db['status'] == 4)
+{
+    $block_status = '5. Allways block new commands. Commands that do not apper in whitelist are automatically blocked. (<b>Automatically enabled after learning period for 3 or 7 days is over</b>).'; 
+} else if ($db['status'] == 10)
+{
+    $block_status = '10. Enable learning mode. If learning mode is over you need to swith it to other mode <b>manually<b/>.';
+} else if ($db['status'] == 11)
+{
+    $block_status = "11. Enable learning mode for 3 days starting from: ".$db['status_changed'];
+} else if ($db['status'] == 12)
+{
+    $block_status = "12. Enable learning mode for 7 days starting from: ".$db['status_changed'];
+}
+$smarty->assign("DB_BlockStatus", $block_status);
+
+if ( ($db['perms'] & 4) == 0 )
 {
     $smarty->assign("DB_Alter", "blocked");
 } else {
     $smarty->assign("DB_Alter", $enabled_str);
 }
 
-if ($db['create_perm'] == 0)
+if ( ($db['perms'] & 1) == 0 )
 {
     $smarty->assign("DB_Create", "blocked");
 } else {
     $smarty->assign("DB_Create", $enabled_str);
 }
 
-if ($db['drop_perm'] == 0)
+if ( ($db['perms'] & 2) == 0 )
 {
     $smarty->assign("DB_Drop", "blocked");
 } else {
     $smarty->assign("DB_Drop", $enabled_str);
 }
 
-if ($db['info_perm'] == 0)
+if ( ($db['perms'] & 8) == 0 )
 {
     $smarty->assign("DB_Info", "blocked");
 } else {
     $smarty->assign("DB_Info", $enabled_str);
 }
 
-if ($db['block_q_perm'] == 0)
+if ( ($db['perms'] & 16) == 0 )
 {
     $smarty->assign("DB_BlockQ", "blocked");
 } else {
