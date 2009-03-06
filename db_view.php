32d0d57205c848087c758dbe520fd825884def52
yuli_s <yuli_s@7e47f64f-3c34-0410-9091-801b9fe948be>
2009-03-06 19:42:54 +0000

    Added local database menu.
    git-svn-id: http://svn.code.sf.net/p/greensql/code/trunk/greensql-console@480 7e47f64f-3c34-0410-9091-801b9fe948be

--- db_view.php
+++ db_view.php
@@ -4,9 +4,9 @@ require 'lib.php';
 global $smarty;
 
 $db_id = 0;
-if (isset($_GET['id']))
+if (isset($_GET['db_id']))
 {
-    $db_id = intval($_GET['id']);
+    $db_id = abs(intval($_GET['db_id']));
 }
 if ($db_id == 0)
 {
@@ -18,15 +18,16 @@ $db  = get_database($db_id);
 
 $smarty->assign("databases", $dbs);
 
-$smarty->assign("Name","Vew database - ".$db['db_name']);
+$smarty->assign("Name","View database - ".$db['db_name']);
 $smarty->assign("Page","db_view.tpl");
-$smarty->assign("DB_Name", $db['db_name']);
+$smarty->assign("DB_Name", "Overview");
 $smarty->assign("DB_ProxyName", $db['proxyname']);
 $smarty->assign("DB_ProxyID", $db['proxyid']);
 $smarty->assign("DB_Listener", $db['listener']);
 $smarty->assign("DB_Backend", $db['backend']);
 $smarty->assign("DB_Type", $db['dbtype']);
 $smarty->assign("DB_ID", $db_id);
+$smarty->assign("DB_Menu", get_local_db_menu($db['db_name'], $db_id) );
 
 $enabled_str = "<font color=\"red\">enabled</font>";
 
