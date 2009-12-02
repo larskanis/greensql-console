200b43c20990776572fcdc6366dd54aa7a403156
yuli_s <yuli_s@7e47f64f-3c34-0410-9091-801b9fe948be>
2009-12-02 12:40:46 +0000

    removed unused files
    git-svn-id: http://svn.code.sf.net/p/greensql/code/trunk/greensql-console@604 7e47f64f-3c34-0410-9091-801b9fe948be

--- libs/plugins/function.html_options.php
+++ libs/plugins/function.html_options.php
@@ -1,122 +1,104 @@
 <?php
-/**
- * Smarty plugin
- * @package Smarty
- * @subpackage plugins
+/*
+ * template_lite plugin
+ * -------------------------------------------------------------
+ * Type:     function
+ * Name:     html_options
+ * Purpose:  prints out the options for an html_select item
+ * Credit:   Taken from the original Smarty
+ *           http://smarty.php.net
+ * -------------------------------------------------------------
  */
-
-
-/**
- * Smarty {html_options} function plugin
- *
- * Type:     function<br>
- * Name:     html_options<br>
- * Input:<br>
- *           - name       (optional) - string default "select"
- *           - values     (required if no options supplied) - array
- *           - options    (required if no values supplied) - associative array
- *           - selected   (optional) - string default not set
- *           - output     (required if not options supplied) - array
- * Purpose:  Prints the list of <option> tags generated from
- *           the passed parameters
- * @link http://smarty.php.net/manual/en/language.function.html.options.php {html_image}
- *      (Smarty online manual)
- * @author Monte Ohrt <monte at ohrt dot com>
- * @param array
- * @param Smarty
- * @return string
- * @uses smarty_function_escape_special_chars()
- */
-function smarty_function_html_options($params, &$smarty)
+function tpl_function_html_options($params, &$tpl)
 {
-    require_once $smarty->_get_plugin_filepath('shared','escape_special_chars');
-    
-    $name = null;
-    $values = null;
-    $options = null;
-    $selected = array();
-    $output = null;
-    
-    $extra = '';
-    
-    foreach($params as $_key => $_val) {
-        switch($_key) {
-            case 'name':
-                $$_key = (string)$_val;
-                break;
-            
-            case 'options':
-                $$_key = (array)$_val;
-                break;
-                
-            case 'values':
-            case 'output':
-                $$_key = array_values((array)$_val);
-                break;
+	require_once("shared.escape_chars.php");
+	$name = null;
+	$options = null;
+	$selected = array();
+	$extra = '';
+  
+	foreach($params as $_key => $_val)
+	{
+		switch($_key)
+		{
+			case 'name':
+				$$_key = (string)$_val;
+				break;
+			case 'options':
+				$$_key = (array)$_val;
+				break;
+			case 'values':
+			case 'output':
+				$$_key = array_values((array)$_val);
+				break;
+			case 'selected':
+				$$_key = array_values((array)$_val);      
+				break;
+			default:
+				if(!is_array($_key))
+				{
+					$extra .= ' ' . $_key . '="' . tpl_escape_chars($_val) . '"';
+				}
+				else
+				{
+					$tpl->trigger_error("html_select: attribute '$_key' cannot be an array");
+				}
+				break;
+		}
+	}
 
-            case 'selected':
-                $$_key = array_map('strval', array_values((array)$_val));
-                break;
-                
-            default:
-                if(!is_array($_val)) {
-                    $extra .= ' '.$_key.'="'.smarty_function_escape_special_chars($_val).'"';
-                } else {
-                    $smarty->trigger_error("html_options: extra attribute '$_key' cannot be an array", E_USER_NOTICE);
-                }
-                break;
-        }
-    }
+	$_html_result = '';
+	if (is_array($options))
+	{
+		foreach ($options as $_key=>$_val)
+		{
+			$_html_result .= tpl_function_html_options_optoutput($tpl, $_key, $_val, $selected);      
+		}
+	}
+	else
+	{
+		foreach ((array)$values as $_i=>$_key)
+		{
+			$_val = isset($output[$_i]) ? $output[$_i] : '';
+			$_html_result .= tpl_function_html_options_optoutput($tpl, $_key, $_val, $selected);
+		}
+	 }
 
-    if (!isset($options) && !isset($values))
-        return ''; /* raise error here? */
+	if(!empty($name))
+	{
+		$_html_result = '<select name="' . tpl_escape_chars($name) . '"' . $extra . '>' . "\n" . $_html_result . '</select>' . "\n";
+	}
 
-    $_html_result = '';
-
-    if (isset($options)) {
-        
-        foreach ($options as $_key=>$_val)
-            $_html_result .= smarty_function_html_options_optoutput($_key, $_val, $selected);
-
-    } else {
-        
-        foreach ($values as $_i=>$_key) {
-            $_val = isset($output[$_i]) ? $output[$_i] : '';
-            $_html_result .= smarty_function_html_options_optoutput($_key, $_val, $selected);
-        }
-
-    }
-
-    if(!empty($name)) {
-        $_html_result = '<select name="' . $name . '"' . $extra . '>' . "\n" . $_html_result . '</select>' . "\n";
-    }
-
-    return $_html_result;
-
+	return $_html_result;
 }
 
-function smarty_function_html_options_optoutput($key, $value, $selected) {
-    if(!is_array($value)) {
-        $_html_result = '<option label="' . smarty_function_escape_special_chars($value) . '" value="' .
-            smarty_function_escape_special_chars($key) . '"';
-        if (in_array((string)$key, $selected))
-            $_html_result .= ' selected="selected"';
-        $_html_result .= '>' . smarty_function_escape_special_chars($value) . '</option>' . "\n";
-    } else {
-        $_html_result = smarty_function_html_options_optgroup($key, $value, $selected);
-    }
-    return $_html_result;
+function tpl_function_html_options_optoutput(&$tpl, $key, $value, $selected)
+{
+	if(!is_array($value))
+	{
+		$_html_result = '<option label="' . tpl_escape_chars($value) . '" value="' . tpl_escape_chars($key) . '"';
+		if (in_array($key, $selected))
+		{
+			$_html_result .= ' selected="selected"';
+		}
+		$_html_result .= '>' . tpl_escape_chars($value) . '</option>' . "\n";
+	}
+	else
+	{
+		$_html_result = tpl_function_html_options_optgroup($tpl, $key, $value, $selected);
+	}
+	return $_html_result;
 }
 
-function smarty_function_html_options_optgroup($key, $values, $selected) {
-    $optgroup_html = '<optgroup label="' . smarty_function_escape_special_chars($key) . '">' . "\n";
-    foreach ($values as $key => $value) {
-        $optgroup_html .= smarty_function_html_options_optoutput($key, $value, $selected);
-    }
-    $optgroup_html .= "</optgroup>\n";
-    return $optgroup_html;
+function tpl_function_html_options_optgroup(&$tpl, $key, $values, $selected)
+{
+	$optgroup_html = '<optgroup label="' . tpl_escape_chars($key) . '">' . "\n";
+	foreach ($values as $key => $value)
+	{
+		$optgroup_html .= tpl_function_html_options_optoutput($tpl, $key, $value, $selected);
+	}
+	$optgroup_html .= "</optgroup>\n";
+	return $optgroup_html;
 }
-
-/* vim: set expandtab: */
 
 ?>
